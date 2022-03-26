e9eeccfbb074b2f189ab7143e82ba49089ed6c51
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-03-26 15:50:31 +0100

    test: add additional cases

--- inst/tinytest/test_Fig.R
+++ inst/tinytest/test_Fig.R
@@ -2,6 +2,10 @@ fig <- Fig$new()
 
 # set returns reference to original object
 expect_identical(fig$set("a", 1), fig)
+expect_identical(fig$set("b", 1:3), fig)
+expect_identical(fig$set("c", list("x", 1)), fig)
 
 # get retrieves correct value
 expect_equal(fig$get("a"), 1)
+expect_equal(fig$get("b"), 1:3)
+expect_equal(fig$get("c"), list("x", 1))
