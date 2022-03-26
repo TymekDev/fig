b3011b33cc868568fd1492d0d178c7af8efae77b
Tymoteusz Makowski <t@makowski.sh>
2022-03-26 15:04:00 +0100

    test: use expect_identical for references

--- inst/tinytest/test_Fig.R
+++ inst/tinytest/test_Fig.R
@@ -1,7 +1,7 @@
 fig <- Fig$new()
 
 # set returns reference to original object
-expect_equal(fig$set("a", 1), fig)
+expect_identical(fig$set("a", 1), fig)
 
 # get retrieves correct value
 expect_equal(fig$get("a"), 1)
--- inst/tinytest/test_functions.R
+++ inst/tinytest/test_functions.R
@@ -1,5 +1,5 @@
 # fig_set returns reference to internal fig object
-expect_equal(fig_set("a", 1), fig:::fig)
+expect_identical(fig_set("a", 1), fig:::fig)
 
 # fig_get retrieves correct value
 expect_equal(fig_get("a"), 1)
