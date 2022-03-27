870f908fb2c3a6a348038732cbb896086e070ff1
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-03-27 21:53:51 +0200

    refactor: replace update_env_prefix with configure

---
+++ tests/testthat/test_configure.R
@@ -0,0 +1,33 @@
+test_that("configure returns reference", {
+  fig <- Fig$new()
+  expect_identical(fig$configure("foo"), fig)
+})
+
+test_that("configure validates env_prefix", {
+  fig <- Fig$new()
+  expect_error(fig$configure(env_prefix = c("a", "b")))
+  expect_error(fig$configure(env_prefix = NA_character_))
+  expect_error(fig$configure(env_prefix = 1))
+  expect_error(fig$configure(env_prefix = list()))
+  expect_silent(fig$configure(env_prefix = "foo"))
+})
+
+test_that("configure works for env_prefix", {
+  fig <- Fig$new()$configure(env_prefix = "foo_")
+  with_envvar(list(foo_bar = "a"), expect_equal(fig$get("bar"), "a"))
+  expect_equal(fig$get("bar"), NULL)
+})
+
+test_that("fig_configure works for env_prefix", {
+  fig_configure(env_prefix = "foo_")
+  with_envvar(list(foo_bar = "a"), expect_equal(fig_get("bar"), "a"))
+  expect_equal(fig_get("bar"), NULL)
+  fig_purge()
+})
+
+test_that("fig_configure shares configure arguments", {
+  expect_equal(
+    formalArgs(fig_configure),
+    formalArgs(Fig$new()$configure)
+  )
+})
