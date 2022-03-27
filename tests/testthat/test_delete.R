ba34cbfd71ef13651e064240651115212a6abcbd
Tymoteusz Makowski <t@makowski.sh>
2022-03-27 21:36:38 +0200

    refactor: rename set to store

--- tests/testthat/test_delete.R
+++ tests/testthat/test_delete.R
@@ -1,18 +1,18 @@
 test_that("delete returns reference", {
   fig <- Fig$new()
-  expect_identical(fig$set("foo", 1)$delete("foo"), fig)
+  expect_identical(fig$store("foo", 1)$delete("foo"), fig)
 })
 
 test_that("delete works", {
   fig <- Fig$new()
-  expect_equal(fig$set("foo", 1)$delete("foo")$get("foo"), NULL)
-  fig$set_many(foo = 1, bar = 2)$delete("foo", "bar")
+  expect_equal(fig$store("foo", 1)$delete("foo")$get("foo"), NULL)
+  fig$store_many(foo = 1, bar = 2)$delete("foo", "bar")
   expect_equal(fig$get("foo"), NULL)
   expect_equal(fig$get("bar"), NULL)
 })
 
 test_that("fig_delete works", {
-  fig_set("foo", 1)
+  fig_store("foo", 1)
   fig_delete("foo")
   expect_equal(fig_get("foo"), NULL)
   fig_purge()
--- tests/testthat/test_get.R
+++ tests/testthat/test_get.R
@@ -10,17 +10,17 @@ test_that("get precedence works", {
   fig <- Fig$new("prefix_")
 
   # System environment > manually set value
-  fig$set("foo", 1)
+  fig$store("foo", 1)
   with_envvar(list(prefix_foo = "a"), expect_equal(fig$get("foo"), "a"))
 })
 
 test_that("get works with YAML key notation", {
   fig <- Fig$new()
 
-  fig$set("foo", list(bar = 1))
+  fig$store("foo", list(bar = 1))
   expect_equal(fig$get("foo.bar"), 1)
 
-  fig$set("foo", list(bar = list(baz = 2)))
+  fig$store("foo", list(bar = list(baz = 2)))
   expect_equal(fig$get("foo"), list(bar = list(baz = 2)))
   expect_equal(fig$get("foo.bar"), list(baz = 2))
   expect_equal(fig$get("foo.bar.baz"), 2)
@@ -28,23 +28,23 @@ test_that("get works with YAML key notation", {
 
 test_that("get environment lookup works with YAML key notation", {
   fig <- Fig$new("prefix_")
-  fig$set("foo", list(bar = 1))
+  fig$store("foo", list(bar = 1))
   with_envvar(list(prefix_foo_bar = "a"), expect_equal(fig$get("foo.bar"), "a"))
   expect_equal(fig$get("foo.bar"), 1)
 })
 
 test_that("get split argument works", {
   fig <- Fig$new()
-  fig$set("foo", list(bar = 1))
-  fig$set("foo.bar", 2, FALSE)
+  fig$store("foo", list(bar = 1))
+  fig$store("foo.bar", 2, FALSE)
   expect_equal(fig$get("foo.bar"), 1)
   expect_equal(fig$get("foo.bar", FALSE), 2)
 })
 
 test_that("fig.split option works", {
   fig <- Fig$new()
-  fig$set("foo", list(bar = 1))
-  fig$set("foo.bar", 2, FALSE)
+  fig$store("foo", list(bar = 1))
+  fig$store("foo.bar", 2, FALSE)
   expect_equal(fig$get("foo.bar"), 1)
   with_options(list(fig.split = FALSE), expect_equal(fig$get("foo.bar"), 2))
 })
